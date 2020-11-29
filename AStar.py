b3033e765ef097b4b856c158fc82cbc8dd7220fa
ProWily <53137051+ProWily@users.noreply.github.com>
2020-11-29 23:29:35 +0300

    Update AStar.py

--- AStar.py
+++ AStar.py
@@ -10,6 +10,7 @@
 # calc = new boolean[N];
 # list_ = new Arraylist_<Integer>();
 
+
 N = 10
 G = [[0] * 10] * 10
 estCost = [0] * 10
@@ -21,22 +22,27 @@ list_ = []
 
 
 def hFunc(i):
+    global estCost
     return estCost[i]
 
 
 def retFCost(city):
+    global funcFCost
     return funcFCost[city]
 
 
 def btwCost(first, second):
+    global toCityCost
     return toCityCost[first][second]
 
 
 def retGCost(city):
+    global funcGCost
     return funcGCost[city]
 
 
 def compTwoCities(from_, to):
+    global funcFCost
     newR = retGCost(from_) + btwCost(from_, to) + hFunc(to)
     if newR < funcFCost[to]:
         funcFCost[to] = newR
@@ -48,6 +54,7 @@ def assignFCost(city):
 
 
 def assignGCost(from_, to):
+    global funcGCost
     funcGCost[to] = retGCost(from_) + btwCost(from_, to)
 
 
@@ -75,15 +82,15 @@ def retCity(i):
 
 
 def AStar(Loc):
+    global N, G, estCost, toCityCost, funcFCost, funcGCost, calc, list_
     print("The current location: " + retCity(Loc))
     print(f"F(n):{retFCost(Loc)}= G({retGCost(Loc)}) + H({hFunc(Loc)})\n")
 
-    if Loc == 2:
-        print("now in Bucharest, The goal has been reached")
+    if Loc == 3:
+        print(f"now in {retCity(Loc)}, The goal has been reached")
         return 1
-
     for i in range(N):
-        if G[Loc][i] == True and list_.contains(i):
+        if G[Loc][i] == True and i in list_:
             compTwoCities(Loc, i)
         elif G[Loc][i] == True:
             list_.append(i)
@@ -94,89 +101,85 @@ def AStar(Loc):
 
     print("---------------Expansion--------------")
     for i in list_:
-        print(
-            "|"
-            + retCity(i)
-            + " F(n): "
-            + retFCost(i)
-            + " = G("
-            + retGCost(i)
-            + ") + H("
-            + hFunc(i)
-            + ")|"
-        )
+        print(f"|{retCity(i)} F(n): {retFCost(i)}= G({retGCost(i)}) + H({hFunc(i)})|")
 
     print("----------------------------------------\n\n")
 
     dest = list_[0]
 
-    for i in list_.size():
+    for i in range(len(list_)):
         if retFCost(list_[i]) < retFCost(dest):
             dest = list_[i]
     AStar(dest)
 
 
 def setupGraph():
+    global G
     G[0][1] = G[1][0] = True
-    G[0][3] = G[3][0] = True
-    G[1][5] = G[5][1] = True
-    G[1][7] = G[7][1] = True
-    G[2][8] = G[8][2] = True
-    G[2][5] = G[5][2] = True
-    G[3][4] = G[4][3] = True
+    G[0][5] = G[5][0] = True
+    G[0][6] = G[6][0] = True
+    G[1][2] = G[2][1] = True
+    G[1][3] = G[3][1] = True
+    G[1][6] = G[6][1] = True
+    G[1][9] = G[9][1] = True
+    G[2][3] = G[3][2] = True
+    G[2][4] = G[4][2] = True
+    G[2][6] = G[6][2] = True
+    G[3][9] = G[9][3] = True
     G[4][6] = G[6][4] = True
+    G[4][7] = G[7][4] = True
+    G[5][6] = G[6][5] = True
     G[5][7] = G[7][5] = True
-    G[6][10] = G[10][6] = True
-    G[7][9] = G[9][7] = True
-    G[8][9] = G[9][8] = True
-    G[9][10] = G[10][9] = True
-    G[9][12] = G[12][9] = True
-    G[10][11] = G[11][10] = True
-    G[11][12] = G[12][11] = True
+    G[6][7] = G[7][6] = True
+    G[7][8] = G[8][7] = True
 
 
 def setupEstCost():
-    estCost[0] = 242
-    estCost[1] = 160
-    estCost[2] = 0
-    estCost[3] = 241
-    estCost[4] = 244
-    estCost[5] = 98
-    estCost[6] = 329
-    estCost[7] = 193
-    estCost[8] = 178
-    estCost[9] = 253
-    estCost[10] = 366
-    estCost[11] = 374
-    estCost[12] = 380
+    global estCost
+    estCost[0] = 266
+    estCost[1] = 154
+    estCost[2] = 35
+    estCost[3] = 0
+    estCost[4] = 214
+    estCost[5] = 408
+    estCost[6] = 212
+    estCost[7] = 306
+    estCost[8] = 496
+    estCost[9] = 244
 
 
 def btwCities():
-    toCityCost[0][1] = toCityCost[1][0] = 120
-    toCityCost[0][3] = toCityCost[3][0] = 75
-    toCityCost[1][5] = toCityCost[5][1] = 138
-    toCityCost[1][7] = toCityCost[7][1] = 146
-    toCityCost[2][8] = toCityCost[8][2] = 211
-    toCityCost[2][5] = toCityCost[5][2] = 101
-    toCityCost[3][4] = toCityCost[4][3] = 70
-    toCityCost[4][6] = toCityCost[6][4] = 111
-    toCityCost[5][7] = toCityCost[7][5] = 97
-    toCityCost[6][10] = toCityCost[10][6] = 118
-    toCityCost[7][9] = toCityCost[9][7] = 80
-    toCityCost[8][9] = toCityCost[9][8] = 99
-    toCityCost[9][10] = toCityCost[10][9] = 140
-    toCityCost[9][12] = toCityCost[12][9] = 151
-    toCityCost[10][11] = toCityCost[11][10] = 75
-    toCityCost[11][12] = toCityCost[12][11] = 71
+    global toCityCost
+    toCityCost[0][1] = toCityCost[1][0] = 232
+    toCityCost[0][5] = toCityCost[5][0] = 200
+    toCityCost[0][6] = toCityCost[6][0] = 188
+    toCityCost[1][2] = toCityCost[2][1] = 150
+    toCityCost[1][3] = toCityCost[3][1] = 154
+    toCityCost[1][6] = toCityCost[6][1] = 160
+    toCityCost[1][9] = toCityCost[9][1] = 196
+    toCityCost[2][3] = toCityCost[3][2] = 35
+    toCityCost[2][4] = toCityCost[4][2] = 182
+    toCityCost[2][6] = toCityCost[6][2] = 184
+    toCityCost[3][9] = toCityCost[9][3] = 244
+    toCityCost[4][6] = toCityCost[6][4] = 201
+    toCityCost[4][7] = toCityCost[7][4] = 146
+    toCityCost[5][6] = toCityCost[6][5] = 194
+    toCityCost[5][7] = toCityCost[7][5] = 170
+    toCityCost[6][7] = toCityCost[7][6] = 157
+    toCityCost[7][8] = toCityCost[8][7] = 235
 
 
 def main():
+    global N, G, estCost, toCityCost, funcFCost, funcGCost, calc, list_
+    setupGraph()
+    setupEstCost()
+    btwCities()
+
     print("------------")
-    start = int(input("enter the start"))
+    start = int(input("enter the start : "))
     funcGCost[start] = 0
     assignFCost(start)
     calc[start] = True
-    # list_.append(10)
     list_.append(start)
     AStar(start)
     print("\n\n")
